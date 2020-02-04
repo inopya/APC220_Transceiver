3822c1f51dc93085df033f1a0460c22495ec4cb7
Eulogio Lopez <usuariogit@hotmail.com>
2020-02-04 13:24:53 +0100

    Update config_apc220_v1.0.ino

--- config_apc220_v1.0/config_apc220_v1.0.ino
+++ config_apc220_v1.0/config_apc220_v1.0.ino
@@ -21,6 +21,36 @@
 /*
   
  ===== NOTAS DE LA VERSION =====
+ 
+ >> Cuando se lee la configuracion del modulo se obtiene un linea similar a esta (PARAM 415370 2 9 3 0 )
+
+"PARAM AAAAAA B C D E"
+
+	AAAAAA, es la frecuencia de trabajo del modulo expresada en KHz 
+	Puede oscilar entre 418MHz y 455MHz
+	- en el ejemplo 415.370 MHz 
+
+	B, es la velocidad de transmision de radio frecuencia puede tomar los siguientes valores
+	1 (2400bps), 2 (4800bps), 3 (9600bps), 4 (19200bps)
+	- en el ejemplo 4800bps
+	
+	C, es la potencia de emision, puede tomar valores entre 0 y 9, siendo 9 la mayor potencia
+	- en el ejemplo 9
+	
+	D, velodidad de transferencia entre el modulo y arduino o PC 	, toma valores entre 0 y 6
+	0 (1200bps), 1 (2400bps), 2 (4800bps),3 (9600bps), 4 (19200bps), 5 (38400bps), 6 (57600bps)
+	- en el ejemplo 9600bps
+	
+	E, es el control de paridad de la informacion emitida por RF
+	0 (sin control de paridad), 1 (paridad par), 2 (paridad impar)
+  - sin control de paridad
+
+>> Para grabar informacion se ha de enviar una linea similar...
+   WR 434000 3 9 3 0
+
+  Esta configuracion seria: Frecuencia de emision 434MHz, velocidad RF 9600, 
+  maxima potencia, Puerto serie 9600 y sin control de paridad
+ 
 */
 
 
@@ -110,13 +140,13 @@ void loop()
 void write_config()
 {
   Serial.println(F("ESTABLECIENDO NUEVA CONFIGURACION...\n"));
-  digitalWrite(SET, LOW);         // poner en modo configuracion
+  digitalWrite(SET, LOW);               // poner en modo configuracion
   delay(50);
-  APCport.print("WR 415372 3 9 3 0");  //consultar el datasheet para detalle de los parametros
-  APCport.write(0x0D); 
-  APCport.write(0x0A);
+  APCport.print("WR 415372 3 9 3 0");   // consultar el datasheet para detalle de los parametros
+  APCport.write(0x0D);                  // fin de linea
+  APCport.write(0x0A);                  // y retorno de carro. Similar a hacer un 'println', pero con println no funciona :(
   delay(100);
-  digitalWrite(SET, HIGH);        // volver al modo normal
+  digitalWrite(SET, HIGH);              // volver al modo normal
 }
 
 
@@ -131,8 +161,8 @@ void read_config()
   digitalWrite(SET, LOW);         // poner en modo configuracion
   delay(50);                      // pausa para estabilizar
   APCport.print("RD");            // peticion de datos
-  APCport.write(0x0D);
-  APCport.write(0x0A);
+  APCport.write(0x0D);            // fin de linea
+  APCport.write(0x0A);            // y retorno de carro. Similar a hacer un 'println', pero con println no funciona :(
   delay(100);                     // pausa para estabilizar
   
   while (APCport.available()) {
